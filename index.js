1ed121d93669b6453607e7a3685de309718aedde
Elliot Collins <150339930+itsDipsyProjects@users.noreply.github.com>
2024-10-14 22:48:27 +0200

    init of project

---
+++ index.js
@@ -0,0 +1,12 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+let canvasWidth = 700;
+let canvasHeight = 400;
+
+canvas.width = canvasWidth;
+canvas.height = canvasHeight
+
+function main() {
+        
+}
